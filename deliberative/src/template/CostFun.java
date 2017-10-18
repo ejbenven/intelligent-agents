7b0553bb60a473bc8606277b553683ee7f564a2a
ejbenven <eloi.benvenuti@gmail.com>
2017-10-19 01:55:16 +0200

    created comparator class

---
+++ deliberative/src/template/CostFun.java
@@ -0,0 +1,46 @@
+package template;
+
+import logist.task.Task;
+import logist.topology.Topology.City;
+import java.util.Comparator;
+
+
+class CostFun implements Comparator<State> {
+    /* Implement an heuristic to rank the node from the one that we expect
+     * will have the smallest cost to the most expensive one.
+     * To do so we compute the smallest distance to either delivery or pickup
+     */
+    
+    
+    double costPerKm;
+
+    public CostFun (double costPerKm) {
+        this.costPerKm = costPerKm;
+    }
+
+    @Override
+    public int compare (State state1, State state2) {
+        int dist1 = 0;
+        int dist2 = 0;
+
+
+        for (AgentTask task : state1.getAgentTaskList() ) {
+
+        }
+        
+        for (AgentTask task : state1.getCityTasksList() ) {
+
+        }
+
+        for (AgentTask task : state2.getAgentTaskList() ) {
+
+        }
+
+        for (AgentTask task : state2.getCityTasksList() ) {
+
+        }
+
+        return Double.compare(state1.getCost() + dist1*costPerKm, state2.getCost() + dist2*costPerKm);
+    }
+
+}
