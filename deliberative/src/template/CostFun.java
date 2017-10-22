981515d798ebaf30caba5bfdfeb47e86998229a6
ejbenven <eloi.benvenuti@gmail.com>
2017-10-22 19:27:55 +0200

    implemented bfs

--- deliberative/src/template/CostFun.java
+++ deliberative/src/template/CostFun.java
@@ -8,7 +8,8 @@ import java.util.Comparator;
 class CostFun implements Comparator<State> {
     /* Implement an heuristic to rank the node from the one that we expect
      * will have the smallest cost to the most expensive one.
-     * To do so we compute the smallest distance to either delivery or pickup
+     * To do so we compute the sum of the distance to every delivery city
+     * and to every pickup city
      */
     
     
@@ -22,21 +23,24 @@ class CostFun implements Comparator<State> {
     public int compare (State state1, State state2) {
         int dist1 = 0;
         int dist2 = 0;
+        City curr1 = state1.getCurrentCity();
+        City curr2 = state2.getCurrentCity();
 
 
         for (Task task : state1.getAgentTaskList() ) {
-
+            dist1 += curr1.distanceTo(task.deliveryCity);
         }
         
         for (Task task : state1.getCityTasksList() ) {
-
+            dist1 += curr1.distanceTo(task.pickupCity);
         }
 
         for (Task task : state2.getAgentTaskList() ) {
-
+            dist2 += curr2.distanceTo(task.deliveryCity);
         }
 
         for (Task task : state2.getCityTasksList() ) {
+            dist2 += curr2.distanceTo(task.pickupCity);
 
         }
 
--- deliberative/src/template/DeliberativeTemplate.java
+++ deliberative/src/template/DeliberativeTemplate.java
@@ -16,6 +16,7 @@ import java.util.PriorityQueue;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 
 /**
  * An optimal planner for one vehicle.
@@ -64,7 +65,7 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
             break;
         case BFS:
             // ...
-            plan = naivePlan(vehicle, tasks);
+            plan = BfsPlan(vehicle, tasks);
             break;
         default:
             throw new AssertionError("Should not happen.");
@@ -72,6 +73,126 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
         return plan;
     }
     
+    private Plan BfsPlan (Vehicle vehicle, TaskSet tasks) {
+        City currentCity = vehicle.getCurrentCity();
+        Plan plan = new Plan(currentCity);
+        
+        TaskSet carriedTasksts = vehicle.getCurrentTasks();
+        TaskSet cityTasksts = TaskSet.intersectComplement(tasks,carriedTasksts);
+        
+        double costPerKm = vehicle.costPerKm();
+        double minCost = Double.POSITIVE_INFINITY;
+        State endState = null;
+
+        //We initialise the starting state
+        Set<Task> carriedTasks = new HashSet<Task>();
+        for (Task task : carriedTasksts ) {
+            carriedTasks.add(task);
+        }
+
+        Set<Task> cityTasks = new HashSet<Task>();
+        for (Task task : cityTasksts ) {
+             cityTasks.add(task);
+        }
+        
+        List<AgentAction> agentActionList = new ArrayList<AgentAction>();
+        State startState = new State(currentCity, carriedTasks, cityTasks, 
+                carriedTasksts.weightSum(), 0., agentActionList);
+
+
+        //Create a priority queue that order the element according to costFun
+        LinkedList<State> Q = new LinkedList<State>();
+
+        //Set of all the states that have already been visited
+        HashSet<State> C = new HashSet<State>();
+        //We store the best cost for each state
+        HashMap<State,Double> stateBestCost = new HashMap<State,Double>();
+        State sbc = null;
+        int it = 0;
+        double costMemory;
+        Q.add(startState);
+        do {
+            //Break if Q is empty (means that we looked at all the paths)
+            if (Q.isEmpty())
+                break;
+            
+            it++;
+
+            //Get first node in Q
+            State currState = Q.poll();
+
+            //We check that we didn't already evaluate this state and, if yes
+            //if our new evaluation of current state has a lower cost
+            if (C.contains(currState))
+                continue;
+            else
+                C.add(currState);
+
+            
+            //Check if current state is endState
+            if (currState.getCityTasksList().isEmpty() && currState.getAgentTaskList().isEmpty()){
+                //check if this endState is better
+                if (currState.getCost() < minCost) {
+                    minCost = currState.getCost();
+                    endState = currState;
+                }
+                continue;
+            }
+
+            //We loop over all the actions possible, ie all the pickup 
+            //and deliver available in this current state
+            State nextState = null;
+            double nCost;
+            //Set<Task> newCity = new HashSet<Task>();
+            //Set<Task> newAgent = new HashSet<Task>();
+            for (Task task : currState.getAgentTaskList() ) {
+                //Compute next state
+                nextState = currState.clone();
+                //Action of delivering the task
+                AgentAction aAction = new AgentAction(false, true, task.deliveryCity, task.pickupCity, task);
+                //Cost of performing the actoin
+                nCost = currState.getCost() + currState.getCurrentCity().distanceTo(task.deliveryCity) * costPerKm;
+                //Remove the task since it's delivered
+                nextState.removeAgentTask(task);
+                //update cost
+                nextState.setCost(nCost);
+                //Add the action to the list
+                nextState.addAgentAction(aAction);
+                nextState.setCurrentCity(task.deliveryCity);
+                nextState.setWeight(nextState.getWeight() - task.weight);
+
+                //Enqueue it
+                Q.add(nextState);
+            }
+            for (Task task : currState.getCityTasksList() ) {
+                //Compute next state
+                if(vehicle.capacity() < currState.getWeight() + task.weight)
+                    continue;
+
+                nextState = currState.clone();
+
+                AgentAction aAction = new AgentAction(true, false, task.deliveryCity, task.pickupCity, task);
+
+                nCost = currState.getCost() + currState.getCurrentCity().distanceTo(task.pickupCity) * costPerKm;
+
+                nextState.removeCityTask(task);
+                nextState.addAgentTask(task);
+
+                nextState.setCost(nCost);
+                nextState.addAgentAction(aAction);
+                nextState.setCurrentCity(task.pickupCity);
+                nextState.setWeight(nextState.getWeight() + task.weight);
+
+                //Enqueue it
+                Q.add(nextState);
+            }
+
+        }while(true);
+        System.out.println(it);
+        plan = stateToPlan(vehicle, endState, plan);
+        return plan;
+    }
+
     private Plan AStarPlan (Vehicle vehicle, TaskSet tasks) {
         City currentCity = vehicle.getCurrentCity();
         Plan plan = new Plan(currentCity);
@@ -276,6 +397,7 @@ public class DeliberativeTemplate implements DeliberativeBehavior {
             // This cannot happen for this simple agent, but typically
             // you will need to consider the carriedTasks when the next
             // plan is computed.
+            // With our implementation we don't need to.
         }
     }
 }
